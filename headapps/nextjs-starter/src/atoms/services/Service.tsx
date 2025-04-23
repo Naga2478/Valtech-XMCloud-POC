8160eef8fd50bde38e6773b1e57ff0a1b578dcb1
Nagaraju Chatta <nagaraju.chatta@valtech.com>
2025-04-23 17:45:22 +0530

    formatting FE files

--- headapps/nextjs-starter/src/atoms/services/Service.tsx
+++ headapps/nextjs-starter/src/atoms/services/Service.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
-import { ComponentParams, Field, ImageField, Text as JssText, NextImage as JssImage } from '@sitecore-jss/sitecore-jss-nextjs';
-
+import {
+  ComponentParams,
+  Field,
+  ImageField,
+  Text as JssText,
+  NextImage as JssImage,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 
 export interface ServiceFields {
-    Heading:Field<string>;
-    Description:Field<string>;
-    Image:ImageField;
+  Heading: Field<string>;
+  Description: Field<string>;
+  Image: ImageField;
 }
 
-export type ServiceProps = {  
+export type ServiceProps = {
   params: ComponentParams;
   fields: ServiceFields;
-}
+};
 
 export const Default = (props: ServiceProps): JSX.Element => {
-
-  return (    
-      <div className="col-lg-4">
-        <div className="icon-box">
-            <JssImage field={props.fields.Image} />
-        </div>  
-        <h4><JssText field={props.fields.Heading}/></h4> 
-        <p><JssText field={props.fields.Description}/></p>     
+  return (
+    <div className="col-lg-4">
+      <div className="icon-box">
+        <JssImage field={props.fields.Image} />
       </div>
+      <h4>
+        <JssText field={props.fields.Heading} />{' '}
+      </h4>
+      <p>
+        <JssText field={props.fields.Description} />
+      </p>
+    </div>
   );
 };
--- headapps/nextjs-starter/src/components/Services/ServicesTeaser.tsx
+++ headapps/nextjs-starter/src/components/Services/ServicesTeaser.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ServiceFields, ServiceProps, Default as Service } from 'src/atoms/services/Service';
 
-interface ServicesTeaserFields{
-    Services: Array<Services>;
+interface ServicesTeaserFields {
+  Services: Array<Services>;
 }
 
-interface Services{
-  fields: ServiceFields
+interface Services {
+  fields: ServiceFields;
 }
 
 interface ServicesTeaserProps {
@@ -16,17 +16,17 @@ interface ServicesTeaserProps {
   fields: ServicesTeaserFields;
 }
 
-export const Default = (props: ServicesTeaserProps): JSX.Element => {  
+export const Default = (props: ServicesTeaserProps): JSX.Element => {
   return (
     <section id="services">
       <div className="container">
         <div className="row">
-        {props.fields.Services.map((service,i)=>{
+          {props.fields.Services.map((service, i) => {
             const serviceProps: ServiceProps = {
               params: props.params,
-              fields: service.fields
-            }
-            return <Service key={i} {...serviceProps} />
+              fields: service.fields,
+            };
+            return <Service key={i} {...serviceProps} />;
           })}
         </div>
       </div>
